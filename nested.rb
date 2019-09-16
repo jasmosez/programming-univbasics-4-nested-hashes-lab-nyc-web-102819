09eb8834295ab0e05583f99b9f421cb3c34c1e7f
James Schaffer <jas.schaffer@gmail.com>
2019-09-16 19:00:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  
+  return programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
