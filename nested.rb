aa4730dbfa39b7f361033184d283fdefb8a43c20
James Schaffer <jas.schaffer@gmail.com>
2019-09-16 19:04:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto].fetch {|k| k={
+  programmer_hash.fetch(:yukihiro_matsumoto) {|k| k={
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }}
